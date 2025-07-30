f1e13f73e5db9c9be7c308c63305a01c7f79c3a8
gikdev <m.m.bhrm85@gmail.com>
2025-07-30 11:54:17 +0330

    Fixed some stuff

--- src/app/(home)/Footer.tsx
+++ src/app/(home)/Footer.tsx
@@ -2,6 +2,6 @@ import { section } from "#/shared/skins"
 
 export const Footer = () => (
   <footer className={section({ centerish: true })}>
-    <p className="text-body-sm">تمام حقوق محفوظ است - ۱۴۰۴</p>
+    <p className="text-body-sm">تمام حقوق محفوظ است © ۱۴۰۴</p>
   </footer>
 )
